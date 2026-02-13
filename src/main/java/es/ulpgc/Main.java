c24a758e6408a81ea9d8ccad3264008a776471c8
Sara Carri <saracarritome@gmail.com>
2026-02-13 13:56:28 +0000

    Add new feature

--- src/main/java/es/ulpgc/Main.java
+++ src/main/java/es/ulpgc/Main.java
@@ -2,6 +2,6 @@ package es.ulpgc;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Hello!");
     }
 }
